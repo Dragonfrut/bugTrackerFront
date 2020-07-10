df03001ecd07674b663d9b3970139ac7e90c7e31
mark.ricard <mark.ricard@itas.ca>
2020-07-09 21:07:29 -0700

    added dropdown for project and assigned users

--- JS/addBug.js
+++ JS/addBug.js
@@ -1,5 +1,14 @@
 
 $(document).ready(function() {
     getSeverity();
+    getProjects();
+    $('#project').change(function(){
+        getAssignedUsers();
+    })
+    
+    $('#bugSubmit').submit(function(event){
+        event.preventDefault();
+        console.log('submit the bug')
+    })
 
 });
